06c20952270e2692de3a30dc24381de6135486eb
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-23 13:02:06 +0300

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import './App.css'
+import Layout from "./components/Layout"
 
 function App() {
   return (
-    <div className="h-screen flex flex-col overflow-hidden">
-      <header className="border-b border-zinc-500 p-4 text-center">
-        <div className="container mx-auto">App Header</div>
-      </header>
-
-      <main className="flex-1 overflow-y-auto">
-        <div className="h-full flex flex-col md:flex-row">
-          <aside className="w-full md:w-64 order-2 md:order-1 border-r border-zinc-500 p-4">
-            Left Sidebar Content
-          </aside>
-
-          <div className="w-full md:flex-1 order-1 md:order-2 p-4">
-            Primary Content
-          </div>
-
-          <aside className="w-full md:w-72 order-3 md:order-3 border-l border-zinc-500 p-4">
-            Secondary Column
-          </aside>
-        </div>
-      </main>
-
-      <footer className="border-t border-zinc-500 p-4 text-center">
-        <div className="container mx-auto">App Footer</div>
-      </footer>
-    </div>
+    <Layout/>
   )
 }
 
