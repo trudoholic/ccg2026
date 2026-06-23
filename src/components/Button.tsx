98da5301e523111b38d511d0844c8bc5634ea2db
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-23 14:19:17 +0300

    Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import type {ComponentProps} from "react"
+import {twMerge} from 'tailwind-merge'
+
+type Variant = "primary" | "secondary" | "gost-destructive" | "red" | "green"
+
+type ButtonProps = {
+  variant?: Variant
+} & ComponentProps<"button">
+
+export function Button({variant = "primary", className, ...props}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        "transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed",
+        getVariantStyles(variant),
+        className,
+      )}
+    />
+  )
+}
+
+function getVariantStyles(variant: Variant) {
+  switch (variant) {
+    case "primary":
+      return "bg-violet-600 hover:bg-violet-500"
+    case "red":
+      return "bg-red-600 hover:bg-red-500"
+    case "green":
+      return "bg-green-600 hover:bg-green-500"
+    case "secondary":
+      return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400"
+    case "gost-destructive":
+      return "hover:bg-red-800 text-red-800 hover:text-red-200"
+    default:
+      throw new Error(`Invalid variant ${(variant)}`) // satisfies never
+  }
+}
--- src/components/MainControls.tsx
+++ src/components/MainControls.tsx
@@ -1,30 +1,22 @@
-import { useFlowStore } from '../store/useFlowStore'
+import {useFlowStore} from "../store/useFlowStore"
+import {Button} from "./Button"
 
 function MainControls() {
   const decreaseCount = useFlowStore(s => s.decreaseCount)
   const increaseCount = useFlowStore(s => s.increaseCount)
 
-  const buttonClass = `
-    transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed
-    bg-violet-600 hover:bg-violet-500
-    `
-
   return (
     <div className="flex flex-col gap-1">
-      <button
-        type="button"
-        className={buttonClass}
+      <Button
         onClick={decreaseCount}
-      >
-        Decrease Count
-      </button>
-      <button
-        type="button"
-        className={buttonClass}
+        variant={"red"}
+      >Decrease Count</Button>
+
+      <Button
         onClick={increaseCount}
-      >
-        Increase Count
-      </button>
+        variant={"green"}
+      >Increase Count</Button>
+
     </div>
   )
 }
