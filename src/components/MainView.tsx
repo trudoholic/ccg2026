6f92ee1a3670b136d0b1231c660394ed3f186f99
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-29 20:25:55 +0300

    Layout 2

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -5,22 +5,45 @@ function MainView() {
   const players = usePlayersStore(s => s.players)
 
   return (
-    <>
+    <div className="flex flex-col gap-2">
+
       {players.length? (
           <div className="flex flex-row gap-2">
             {players.map((p, i) => (
-              <PlayerView
-                key={p.id}
-                idx={i}
-                name={p.name}
-                score={p.score}
-              />
+              <div key={p.id} className="flex-1">
+                <PlayerView
+                  idx={i}
+                  name={p.name}
+                  score={p.score}
+                />
+              </div>
             ))}
           </div>
       ): (
         <h1>Start New Game</h1>
       )}
-    </>
+
+      {players.length? (
+        <div className="flex flex-row gap-2">
+          {players.map((p, i) => (
+            <div key={p.id} className="flex-1 p-4 border border-zinc-500">
+              {`Hand ${i}`}
+            </div>
+          ))}
+        </div>
+      ): null}
+
+      {players.length? (
+        <div className="flex flex-row gap-2">
+          {players.map((p, i) => (
+            <div key={p.id} className="flex-1 p-4 border border-zinc-500">
+              {`Keep ${i}`}
+            </div>
+          ))}
+        </div>
+      ): null}
+
+    </div>
   )
 }
 
